2a34ea4750aa7bc3f3ef00accb0568178789bb17
Samuel Braun <slb2250@columbia.edu>
2023-03-05 15:16:30 -0500

    sort.c fleshed out, needs testing

--- src/sort.c
+++ src/sort.c
@@ -8,8 +8,96 @@
 #define MAX_STRLEN     64 // Not including '\0'
 #define MAX_ELEMENTS 1024
 
+void display_usage(char flag) {
+	fprintf(stderr, "Error: Unknown option \'-%c\' received.\n"
+                        "Usage: ./sort [-i|-d] [filename]\n"
+                        "   -i: Specifies the input contains ints.\n"
+                        "   -d: Specifies the input contains doubles.\n"
+                        "   filename: The file to sort. If no file is "
+                        "supplied, input is read from\n"
+                        "             stdin.\n"
+                        "   No flags defaults to sorting strings.\n", flag);
+}
+
 int main(int argc, char **argv) {
-   	// main method
-	       
+
+	int dflag = 0;
+	int iflag = 0;
+	int c;
+	opterr = 0; // equivalent to : in bash
+
+	while ((c = getopt(argc, argv, "di")) != -1) {
+		switch(c){
+			case 'd':
+				dflag = 1;
+				break; // needed?
+			case 'i':
+				iflag = 1;
+				break;
+			case '?':
+				display_usage(optopt);
+				return EXIT_FAILURE;
+			default:
+				return EXIT_FAILURE;
+		}
+	
+	}
+
+	// tests for multiple filenames
+	if (argc - 1 - optind > 0) {
+		fprintf(stderr, "Error: Too many files specified.\n");
+		return EXIT_FAILURE;
+	}
+
+	// tests for too many valid flags
+	if (dflag + iflag > 1) {
+		fprintf(stderr, "Error: Too many flags specified.\n");
+	}
+
+	char buf[MAX_ELEMENTS][MAX_STRLEN];
+
+	FILE *infile;
+	// tests for invalid filename
+	if (optind == 1) {
+		infile = fopen(argv[optind], "r");
+		if (infile == NULL) {
+			fprintf(stderr, "Error: Cannot open \'%s\'.%s.", argv[optind], strerror(errno));
+			return EXIT_FAILURE;
+		}
+	}
+	else {
+		infile = stdin;
+	}
+	
+	// when we do fgets, is there anything read into buf[i]? (what if loop is asking)
+	for (int i = 0; i < MAX_ELEMENTS && fgets(buf[i], MAX_STRLEN, infile) != NULL; i++) {
+		// replaces \n with \0 in strings
+		char *eoln = strchr(buf[i], '\n');
+		if (eoln != NULL) {
+			*eoln = '\0';
+		}
+	}
+
+	// passing buf into quicksort
+	if (dflag == 1) {
+		quicksort((void *) buf, MAX_ELEMENTS, MAX_STRLEN, dbl_cmp);
+	}
+	else if (iflag == 1) {
+		quicksort((void *) buf, MAX_ELEMENTS, MAX_STRLEN, int_cmp);
+	}
+	else {
+		quicksort((void *) buf, MAX_ELEMENTS, MAX_STRLEN, str_cmp);
+	}
+
+	// prints buf to stdout and closes infile
+	fputs(buf, stdout);
+	fclose(infile);
+
 	return EXIT_SUCCESS;
+
+	// read data, takes in filename and outputs array
+        // replace \n with \0
+        //
+        // filestream?
+        // fputs string into a filestream: stdout, stdin, stderr, file itself
 }
