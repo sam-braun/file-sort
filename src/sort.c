955fa1f687840ae71b3b822636b4a89d861b18c8
Samuel Braun <slb2250@columbia.edu>
2023-03-05 19:01:55 -0500

    more in Uris

--- src/sort.c
+++ src/sort.c
@@ -78,15 +78,17 @@ int main(int argc, char **argv) {
 	printf("passed error-checking\n");
 	
 	// when we do fgets, is there anything read into buf[i]? (what if loop is asking)
-	for (int i = 0; i < MAX_ELEMENTS && fgets(buf[i], MAX_STRLEN, infile) != NULL; i++) {
-		
+	for (int i = 0; i < MAX_ELEMENTS; i++) {
 		printf("in fgets loop\n");
 
 		// replaces \n with \0 in strings
-		char *eoln = strchr(buf[i], '\n');
-		if (eoln != NULL) {
-			*eoln = '\0';
+		while (fgets(buf[i], MAX_STRLEN, infile) != NULL) {
+			char *eoln = strchr(buf[i], '\n');
+			if (eoln != NULL) {
+				*eoln = '\0';
+			}
 		}
+		fprintf(stdout, "%s\n", (char *) buf[i]);
 	}
 
 	printf("passed fgets loop\n");
